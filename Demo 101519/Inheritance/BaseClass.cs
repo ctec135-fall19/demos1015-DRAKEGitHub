16d86b3f87587d4d310134941b67fe85acce1011
Allison Drake <allisonnicoledrake@gmail.com>
2019-10-15 15:21:29 -0700

    tuesday Lab 10/15/2019

---
+++ Demo 101519/Inheritance/BaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class BaseClass
+    {
+        // field inheritance 
+        private int basePrivateInt;
+        protected int baseProtectedInt;
+
+        // Constructors for field inheritance (set the values)
+        public BaseClass() { }
+        public BaseClass(int privateInt, int protectedInt)
+        {
+            this.basePrivateInt = privateInt;
+            this.baseProtectedInt = protectedInt;
+        }
+
+        // methods inheritance
+        public void BaseMethod()
+        {
+            Console.WriteLine("BaseClass.BaseMethod");
+        }
+
+        // use virtual to override 
+        public virtual void PrintState()
+        {
+            Console.WriteLine("BaseClass object");
+            Console.WriteLine("\tbasePrivateInt: {0}", basePrivateInt);
+            Console.WriteLine("\tbaseProtectedInt: {0}", baseProtectedInt);
+            Console.WriteLine();
+        }
+        
+    }
+}
---
+++ Demo 101519/Inheritance/ChildClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class ChildClass : BaseClass
+    {
+        // fields
+        protected int childProtectedInt;
+
+        // Constructors
+        public ChildClass() { }
+        public ChildClass(int basePrivateInt, int baseProtectedInt, 
+            int childProtectedInt)
+            : base(basePrivateInt, baseProtectedInt)
+        {
+            this.childProtectedInt = childProtectedInt;
+        }
+
+        // methods
+        public void ChildMethod()
+        {
+            Console.WriteLine("ChildClass.ChildMethod");
+        }
+
+        // use override when there are two classes with the same method
+        // virtual and override (virtual = base, override = child)
+        // I have children classes that may but don't have to 
+        // override this method. Override the parent method
+        public override void PrintState()
+        {
+            Console.WriteLine("ChildClass object");
+            // Console.WriteLine("\tbasePrivateInt: {0}", basePrivateInt);
+            // Console.WriteLine("\tbaseProtectedInt: {0}", baseProtectedInt);
+            Console.WriteLine("\tchildProtectedInt: {0}", childProtectedInt);
+            base.PrintState();
+            Console.WriteLine();
+        }
+    }
+}
